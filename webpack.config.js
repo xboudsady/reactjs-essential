4f0ecde46c0a439d727e41f2f03bc923c906f2e7
Xayaseth Boudsady <boudsadyx@gmail.com>
2018-05-08 22:21:13 -0700

    Updated files

--- webpack.config.js
+++ webpack.config.js
@@ -7,19 +7,19 @@ module.exports = {
         filename: "bundle.js",
         publicPath: "assets"
     },
-    devServers: {
+    devServer: {
         inline: true,
         contentBase: './dist',
         port: 3000
     },
     module: {
-        loaders: {
-            test: /\.js/,
+        loaders: [{
+            test: /\.js$/,
             exclude: /(node_modules)/,
             loader: ["babel-loader"],
             query: {
                 presets: ["latest", "stage-0", "react"]
             }
-        }
+        }]
     }
 }
